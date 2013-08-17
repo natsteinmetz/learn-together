a8121d8f762150bb58ceda55cf9f57f3a6a28029
natsteinmetz <nathalie.steinmetz@gmail.com>
2013-08-17 13:07:09 -0700

    setup project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearnTogether::Application.config.secret_token = '2615fa43e0b4498ab964c176bd180457b49abf6b47ff8d209ee1d47c1d2580d85343ba61f069b77c6dcb87a7a438d3bb3b0d5b9c4ccb9d3329b0f1134203b1f2'
